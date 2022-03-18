e07bfc1f469ee8f7424aff106a58ae06e4a7a80d
Prabhneet Narula <imailcode@gmail.com>
2022-03-18 11:31:04 +0530

    Database Created.

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,5 +1,15 @@
+final String tableTransactions = "transaction";
+
+class TransactionFields {
+  static final List<String> values = [colId, colTitle, colAmount, colDate];
+  static const String colId = "_id";
+  static const String colTitle = "title";
+  static const String colAmount = "amount";
+  static const String colDate = "date";
+}
+
 class Transaction {
-  final String id;
+  final int id;
   final String title;
   final int amount;
   final DateTime date;
@@ -16,22 +26,29 @@ class Transaction {
       required this.amount,
       required this.date});
 
+  Transaction copy({int? id, String? title, int? amount, DateTime? date}) =>
+      Transaction(
+          id: id ?? this.id,
+          title: title ?? this.title,
+          amount: amount ?? this.amount,
+          date: date ?? this.date);
+
   //Now, let's create a function that will turn our data into Map
   //Make sure that the key used here have the same name as the table column
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJson() {
     return ({
-      'id': id,
-      'title': title,
-      'amount': amount,
-      'creationDate': date.toIso8601String()
+      TransactionFields.colId: id,
+      TransactionFields.colTitle: title,
+      TransactionFields.colAmount: amount,
+      TransactionFields.colDate: date.toIso8601String()
     });
   }
 
-  factory Transaction.fromMap(Map<String, dynamic> map) {
+  static Transaction fromJson(Map<String, Object?> json) {
     return Transaction.withId(
-        id: map['id'],
-        title: map['title'],
-        amount: map['amount'],
-        date: DateTime.parse(map['date']));
+        id: json[TransactionFields.colId] as int,
+        title: json[TransactionFields.colTitle] as String,
+        amount: json[TransactionFields.colAmount] as int,
+        date: DateTime.parse(json[TransactionFields.colDate] as String));
   }
 }
--- lib/providers/HomePageProvider.dart
+++ lib/providers/HomePageProvider.dart
@@ -1,19 +1,23 @@
+import 'package:expense_tracker/database/transactionListDb.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import '../widgets/newTransaction.dart';
 
 class HomePageProvider with ChangeNotifier {
   final List<Transaction> userTransactions = [];
-
+  int id = 0;
   void addNewTransaction(String txTitle, int txAmount, DateTime chosenDate) {
     final newTx = Transaction(
-        id: DateTime.now().toString(),
-        title: txTitle,
-        amount: txAmount,
-        date: chosenDate);
+        id: id++, title: txTitle, amount: txAmount, date: chosenDate);
     userTransactions.add(newTx);
     notifyListeners();
   }
+
+  //Using video
+  // updateTransactionList() {
+  //   userList = DatabaseHelper.instance.getTransactionList();
+  //   notifyListeners();
+  // }
 
   List<Transaction> get recentTransactions {
     return userTransactions.where((tx) {
@@ -37,7 +41,7 @@ class HomePageProvider with ChangeNotifier {
         });
   }
 
-  void deleteTransaction(String id) {
+  void deleteTransaction(int id) {
     userTransactions.removeWhere((tx) => tx.id == id);
     notifyListeners();
   }
